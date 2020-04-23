45c613919831ea2da8ddf1efe0dbc113eea86b27
jamesnitz <jamesanitz2@gmail.com>
2020-04-23 14:37:54 -0500

    Index and create working

--- bookshelf/Controllers/BooksController.cs
+++ bookshelf/Controllers/BooksController.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using bookshelf.Data;
 using bookshelf.Models;
+using bookshelf.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace bookshelf.Controllers
 {
+    [Authorize]
     public class BooksController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -21,42 +26,71 @@ namespace bookshelf.Controllers
             _context = context;
         }
         // GET: Books
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var user = await GetCurrentUserAsync();
+            var books = await _context.Book
+                .Where(b => b.ApplicationuserId == user.Id)
+                .Include(b => b.BookGenres)
+                    .ThenInclude(bg => bg.Genre)
+                .ToListAsync();
+            return View(books);
         }
 
         // GET: Books/Details/5
-        public ActionResult Details(int id)
+        public async Task<ActionResult> Details(int id)
         {
             return View();
         }
 
         // GET: Books/Create
-        public ActionResult Create()
+        public async Task<ActionResult> Create()
         {
-            return View();
+            var genreOptions = await _context.Genre
+                .Select(g => new SelectListItem() { Text = g.Name, Value = g.Id.ToString() })
+                .ToListAsync();
+
+            var viewModel = new BookFormViewModel();
+
+            viewModel.GenreOptions = genreOptions;
+
+            return View(viewModel);
         }
 
         // POST: Books/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<ActionResult> Create(BookFormViewModel bookViewModel)
         {
             try
             {
-                // TODO: Add insert logic here
+                var user = await GetCurrentUserAsync();
 
+                var book = new Book
+                {
+                    Title = bookViewModel.Title,
+                    Author = bookViewModel.Author,
+                    ApplicationuserId = user.Id
+                };
+                book.BookGenres = bookViewModel.SelectedGenreIds.Select(gi =>
+                
+                    new BookGenre()
+                    {
+                        Book = book,
+                        GenreId = gi
+                    }).ToList();
+                _context.Book.Add(book);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
                 return View();
             }
         }
 
         // GET: Books/Edit/5
-        public ActionResult Edit(int id)
+        public async Task<ActionResult> Edit(int id)
         {
             return View();
         }
@@ -64,7 +98,7 @@ namespace bookshelf.Controllers
         // POST: Books/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<ActionResult> Edit(int id, IFormCollection collection)
         {
             try
             {
@@ -79,7 +113,7 @@ namespace bookshelf.Controllers
         }
 
         // GET: Books/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
             return View();
         }
@@ -87,7 +121,7 @@ namespace bookshelf.Controllers
         // POST: Books/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<ActionResult> Delete(int id, IFormCollection collection)
         {
             try
             {
@@ -100,5 +134,6 @@ namespace bookshelf.Controllers
                 return View();
             }
         }
+        private async Task<ApplicationUser> GetCurrentUserAsync() => await _userManager.GetUserAsync(HttpContext.User);
     }
 }
